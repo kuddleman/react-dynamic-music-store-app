8a8e7e466a720f3d675e8be0dd50c1df0018d81c
Kuddleman <edl1155@gmail.com>
2020-06-17 15:54:35 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="panel-group">
+        <div className="panel panel-primary">
+          <div className="panel-heading"></div>
+          <div className="panel-body"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
